a5cd2cdea1cbfc58c8898807e74437589b9102b6
Anderson Vieira <asvieira.br@gmail.com>
2023-02-22 22:36:48 -0300

    style: component Navigation styles

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,9 +1,27 @@
 import * as S from './styles';
+import MenuIcon from '@mui/icons-material/Menu';
+import Badge, { BadgeProps } from '@mui/material/Badge';
+import IconButton from '@mui/material/IconButton';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
 
 export default function Navigation() {
   return (
     <S.Navbar>
-      <h1>Fake Store</h1>
+      <IconButton>
+        <MenuIcon />
+      </IconButton>
+      <S.Logo>Fake Store</S.Logo>
+      <S.NavbarIcons>
+        <IconButton aria-label="cart">
+          <ShoppingCartIcon/>
+          <Badge badgeContent={0} />
+        </IconButton>
+        <Stack direction="row" spacing={2}>
+          <Avatar alt="Anderson Vieira" src="https://avatars.githubusercontent.com/u/65618765?v=4" />
+        </Stack>
+      </S.NavbarIcons>
     </S.Navbar>
   )
 }
--- src/components/Navigation/styles.tsx
+++ src/components/Navigation/styles.tsx
@@ -7,4 +7,16 @@ export const Navbar = styled('nav')`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  padding: .5rem 1rem;
+  box-shadow: 0 10px 30px 10px ${color.purple['6001']};
+`
+
+export const Logo = styled('a')`
+  font-size: xx-large;
+  font-weight: bold;
+`
+
+export const NavbarIcons = styled('div')`
+  display: flex;
+  gap: 1rem;
 `
