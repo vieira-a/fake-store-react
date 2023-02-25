10e327e8b1923b1ee7eab0d9bb0c4dec68dccda3
Anderson Vieira <asvieira.br@gmail.com>
2023-02-24 23:44:48 -0300

    refactor: update AddCartProp onClick

--- src/components/AddCart/AddCart.tsx
+++ src/components/AddCart/AddCart.tsx
@@ -1,10 +1,9 @@
 import * as S from "./styles";
 import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
-import { ReactNode } from "react";
 
 interface AddCartProps {
   productId: number;
-  onClick: any;
+  onClick: () => void;
 }
 
 export default function AddCart({ productId, onClick }: AddCartProps) {
